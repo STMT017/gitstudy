a3a773f87300e9bdfe3b069e4897ae27cf92207f
STMT017 <sxy2367221250@163.com>
2023-05-06 12:53:10 +0800

    reset the program

--- src/project1/KMP.h
+++
@@ -1,5 +0,0 @@
-#pragma 
-#include<stdio.h>
-#include"UserList.h"
-int accout_match(User user, SqList L);							//ÕËºÅÆ¥Åä,·µ»ØUID
-const char* password_match(User user, SqList L);			//ÃÜÂëÆ¥Åä,·µ»ØÈÕÖ¾
--- src/project1/Web.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include<stdio.h>
-void login_web();
-void user_web();
